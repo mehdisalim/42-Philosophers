454b8b346ff5ee1c4c63100ceb3263d26decea02
mehdisalim <mehdisalimofficial2000@gmail.com>
2023-03-29 23:17:18 +0000

    refactoring the project

--- philo/threads.c
+++ philo/threads.c
@@ -6,7 +6,7 @@
 /*   By: esalim <esalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 10:11:16 by esalim            #+#    #+#             */
-/*   Updated: 2023/03/29 21:05:33 by esalim           ###   ########.fr       */
+/*   Updated: 2023/03/29 21:22:25 by esalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,51 +34,8 @@ void	*philosopher(void *args)
 			&& checker(data)
 			&& thinking(data))
 		;
-	// pthread_mutex_unlock(&data->mutex_eat[0]);
 	mutex(data, pthread_mutex_unlock, 0);
 	pthread_mutex_unlock(data->mutex_eat);
 	pthread_mutex_unlock(data->mutex_print);
 	return (0);
 }
-
-/*
-void	*thread1(void *args)
-{
-	t_data *data = (t_data *)args;
-	int i = -1;
-	int n = data->args[N_PHILOS] / 2;
-	static int returnValue = 1;
-	while(++i < n)
-	{
-		if (data[i].args[N_O_T_E_P_M_E] && data[i].eater[0] >= data[i].args[N_O_T_E_P_M_E])
-			return (&returnValue);
-		if (get_current_time(data[i].update_time_2_die) >= data[i].args[TIME_2_DIE])
-		{
-			pthread_mutex_lock(&data->mutex_print[0]);
-			printf("%ld %d died\n", get_current_time(data[i].start_time), data[i].args[ID]);
-			return (&returnValue);
-		}
-	}
-	return (0);
-}
-
-void	*thread2(void *args)
-{
-	t_data *data = (t_data *)args;
-	int i = data->args[N_PHILOS] / 2 - 2;
-	int n = data->args[N_PHILOS];
-	static int returnValue = 1;
-	while(++i < n)
-	{
-		if (data[i].args[N_O_T_E_P_M_E] && data[i].eater[0] >= data[i].args[N_O_T_E_P_M_E])
-			return (&returnValue);
-		if (get_current_time(data[i].update_time_2_die) >= data[i].args[TIME_2_DIE])
-		{
-			pthread_mutex_lock(&data->mutex_print[0]);
-			printf("%ld %d died\n", get_current_time(data[i].start_time), data[i].args[ID]);
-			return (&returnValue);
-		}
-	}
-	return (0);
-}
-*/
